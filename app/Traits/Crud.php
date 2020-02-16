1cf05a7202ea83d3ebeec93e80992e306ff95db3
Felipe Braiani <felipe.braianitech@gmail.com>
2020-02-16 14:54:14 -0400

    find and get function implemented

--- app/Traits/Crud.php
+++ app/Traits/Crud.php
@@ -10,4 +10,11 @@ trait Crud
     {
         return $this->model->all();
     }
+
+    public function findAndGet($select = ['*'], $with = [], $where = false)
+    {
+        return $this->model->when($where, function ($q) use ($where) {
+            return $q->where($where['column'], $where['operator'], $where['value']);
+        })->select($select)->with($with)->get();
+    }
 }

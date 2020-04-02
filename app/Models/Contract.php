9265c55be3997efcc758826c792e1a5b98270863
Felipe Braiani <felipe.braianitech@gmail.com>
2020-04-01 23:45:55 -0400

    Added Contract Model informations

--- app/Models/Contract.php
+++ app/Models/Contract.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
-
-class Contract extends Model
+class Contract extends BaseModel
 {
-    //
+    protected $fillable = ['service_id', 'client_id', 'due_date', 'amount', 'installments'];
+
+    protected $dates = ['due_date'];
+
+    public function client()
+    {
+        return $this->belongsTo(Client::class);
+    }
+
+    public function service()
+    {
+        return $this->belongsTo(Service::class);
+    }
 }

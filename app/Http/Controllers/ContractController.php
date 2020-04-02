1d2bac5b13bc3b5a445597f053d173ed0bb9f38c
Felipe Braiani <felipe.braianitech@gmail.com>
2020-04-01 23:48:47 -0400

    Contract Service injection on controller

--- app/Http/Controllers/ContractController.php
+++ app/Http/Controllers/ContractController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\ContractService;
 use Illuminate\Http\Request;
 
 class ContractController extends Controller
 {
-    //
+    protected $contractService;
+
+    /**
+     * ContractController constructor.
+     * @param $contractService
+     */
+    public function __construct(ContractService $contractService)
+    {
+        $this->contractService = $contractService;
+    }
+
+
 }
